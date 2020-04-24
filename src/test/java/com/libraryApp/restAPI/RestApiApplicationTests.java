33502f8f6e5df2dd6f61b83025670197dafc85bb
Александр <medvedev.sasha2000@mail.ru>
2020-04-24 18:15:11 +0300

    init project

---
+++ src/test/java/com/libraryApp/restAPI/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libraryApp.restAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
