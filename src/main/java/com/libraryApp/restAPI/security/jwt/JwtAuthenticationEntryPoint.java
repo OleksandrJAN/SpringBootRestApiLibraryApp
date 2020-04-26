165f14ade208a34e96da62de45efe265b99fad56
Александр <medvedev.sasha2000@mail.ru>
2020-04-26 03:18:56 +0300

    Fix error message

--- src/main/java/com/libraryApp/restAPI/security/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/libraryApp/restAPI/security/jwt/JwtAuthenticationEntryPoint.java
@@ -17,6 +17,6 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
             HttpServletResponse httpServletResponse,
             AuthenticationException e
     ) throws IOException, ServletException {
-        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error -> Unauthorized");
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
     }
 }
